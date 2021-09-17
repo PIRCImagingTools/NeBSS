4d60806c80d26daa7a83b589f8ad9dc3e4aca342
WilliamReynolds <wtr8@pitt.edu>
2021-09-17 17:00:57 -0400

    Change exec to eval to maintain same shell. Add option for *.nii* files which will run generate_config.py and then runs generated config

--- startup.sh
+++ startup.sh
@@ -7,12 +7,19 @@ export FSLDIR PATH
 
 case "$1" in 
 config)
-exec python /app/create_config.py "$2" 
+eval python /app/create_config.py "$2" 
 ;;
 *config.json)
-exec python /app/nebss_cl.py "$@" 
+eval python /app/nebss_cl.py "$@" 
 ;;
-*) 
+*.nii*)
+eval python /app/generate_config.py "$@"
+config_file=$(ls -t | grep config.json | head -n 1)
+if [ -e "$config_file" ]; then
+    eval python /app/nebss_cl.py "$config_file"
+fi
+;;
+*)
 echo "No config or image file found. Entering command mode. 
 
 To create a config file: 
